c8ba88402fc262688347eeb7c9c1cb2ca5825237
DESKTOP-5G35IDM\Jesus Marquez <jesmqz@gmail.com>
2023-06-28 10:17:07 -0400

    Disable cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require('json-server')
+const cors = require('cors');
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults([ "noCors" ])
 
 const PORT = process.env.PORT || 8000;
 
 server.use(middlewares)
 server.use(router)
+server.use( cors())
 server.listen(PORT, () => {
   console.log('JSON Server is running')
 })
